177b5db757e0a78f187930b4447947d1004d4f8d
Vasuki t <iamvasukithangaraj@gmail.com>
2025-09-14 21:37:16 +0530

    added clear,delete,evaluate events

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 
-
+// Click events
 let input = document.querySelector("input")
 function addValue(elementValue) {
     input.value += elementValue;
 
 }
 
+// for clear the Values
+function clearValue() {
+    input.value = "";
+
+}
+
+// for delete values
+function deleteValue() {
+    input.value= input.value.slice(0,input.value.length-1)
+}
+
+
+// for evaluate values
+function evaluateValue() {
+    input.value = eval(input.value);
+}
 
 
 
